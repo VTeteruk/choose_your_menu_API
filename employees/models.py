0479b4c71339e9a35438f847ae6610a8de2d976c
Vlad Teteruk <vladteteruk01@gmail.com>
2023-09-22 15:43:40 +0300

    Implement models & make migrations

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Employee(AbstractUser):
+    position = models.CharField(max_length=255)
--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Menu(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name="menus")
+    date = models.DateField(auto_now_add=True)
+    dishes = models.JSONField()
+
+    class Meta:
+        unique_together = ("restaurant", "date")
