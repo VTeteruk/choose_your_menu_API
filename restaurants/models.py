ba8d6b00a09c6eea9156e3b0debe2c8726a6282b
Vlad Teteruk <vladteteruk01@gmail.com>
2023-09-22 16:12:37 +0300

    Add validation to dishes in Menu model

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 
 class Restaurant(models.Model):
@@ -8,10 +9,18 @@ class Restaurant(models.Model):
         return self.name
 
 
+def validate_dishes_format(value) -> None:
+    valid_days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+
+    for day in value.keys():
+        if day.lower() not in valid_days:
+            raise ValidationError(f"'{day}' is not a valid day name. Valid day names are: {', '.join(valid_days)}")
+
+
 class Menu(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name="menus")
     date = models.DateField(auto_now_add=True)
-    dishes = models.JSONField()
+    dishes = models.JSONField(validators=[validate_dishes_format])
 
     class Meta:
         unique_together = ("restaurant", "date")
